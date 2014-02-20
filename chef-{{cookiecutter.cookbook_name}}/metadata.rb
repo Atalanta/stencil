e6907ee4b7d1b54644896ac7208c1e35b8d4471c
Konstantin Lysenko <konstantin@atalanta-systems.com>
2014-02-20 23:56:00 +0700

    Add: chef metadata to cookiecutter chef cookbook template
    References#SDFE-97: Cookbook Generator

---
+++ chef-{{cookiecutter.cookbook_name}}/metadata.rb
@@ -0,0 +1,7 @@
+name             '{{cookiecutter.cookbook_name}}'
+maintainer       '{{cookiecutter.author}}'
+maintainer_email '{{cookiecutter.email}}'
+license          'All rights reserved'
+description      'Installs/Configures {{cookiecutter.cookbook_name}}'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '{{cookiecutter.version}}'
